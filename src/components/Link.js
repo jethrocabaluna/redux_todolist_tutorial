7733065b5d81eb38a7db83cc0d0a2c3be2be2575
Jethro Cabaluna <jethrocabaluna@Jethros-MacBook-Pro.local>
2018-10-02 19:57:09 +0800

    completed basic todolist on redux tutorial

---
+++ src/components/Link.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Link = ({ active, children, onClick }) => {
+  if (active) {
+    return <span>{children}</span>;
+  }
+
+  return (
+    <button
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+Link.propTypes = {
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+export default Link
